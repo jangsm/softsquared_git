0f558a96840bc4a8006639e7d2428ab7f15300df
tmdalsy <tmdalsy@naver.com>
2019-08-26 00:13:12 +0900

    0825 layout

---
+++ Project_ChaChaCha/app/src/main/java/com/example/project_chachacha/template/src/Mypage/Mypage_main.java
@@ -0,0 +1,27 @@
+package com.example.project_chachacha.template.src.Mypage;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.project_chachacha.R;
+
+
+public class Mypage_main extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mypage_main, container, false);
+    }
+
+
+
+}
